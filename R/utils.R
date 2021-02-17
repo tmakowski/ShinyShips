0a6cbfe69f4eab916aa3f72b661b1bf7ff32aaa6
tmakowski <tymek.makowski@gmail.com>
2021-02-17 18:00:22 +0100

    Reindent code

--- R/utils.R
+++ R/utils.R
@@ -5,14 +5,14 @@
 #'
 #' @return A \code{data.frame} with unique rows
 get_unique_rows <- function(df, col_names) {
-    assertthat::assert_that(
-        is.data.frame(df),
-        is.vector(col_names),
-        length(col_names) > 0,
-        all(col_names %in% colnames(df))
-    )
+  assertthat::assert_that(
+    is.data.frame(df),
+    is.vector(col_names),
+    length(col_names) > 0,
+    all(col_names %in% colnames(df))
+  )
 
-    unique_rows <- unique(df[, col_names, drop = FALSE])
-    rownames(unique_rows) <- NULL
-    unique_rows
+  unique_rows <- unique(df[, col_names, drop = FALSE])
+  rownames(unique_rows) <- NULL
+  unique_rows
 }
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,24 +1,24 @@
 context("utils")
 
 test_that("test get_unique_rows", {
-    # Assert check
-    expect_error(get_unique_rows(NULL, "a"))
-    expect_error(get_unique_rows(data.frame(), NULL))
-    expect_error(get_unique_rows(data.frame(), character(0)))
-    expect_error(get_unique_rows(data.frame(a = 1), "b"))
+  # Assert check
+  expect_error(get_unique_rows(NULL, "a"))
+  expect_error(get_unique_rows(data.frame(), NULL))
+  expect_error(get_unique_rows(data.frame(), character(0)))
+  expect_error(get_unique_rows(data.frame(a = 1), "b"))
 
-    # Calculation check
-    df <- data.frame(A = c(1:2, 1:2), B = c("a", "b", "a", "c"))
-    expect_equal(
-        get_unique_rows(df, c("A", "B")),
-        data.frame(A = c(1, 2, 2), B = c("a", "b", "c"))
-    )
-    expect_equal(
-        get_unique_rows(df, "A"),
-        data.frame(A = 1:2)
-    )
-    expect_equal(
-        get_unique_rows(data.frame(A = 1:2), "A"),
-        data.frame(A = 1:2)
-    )
+  # Calculation check
+  df <- data.frame(A = c(1:2, 1:2), B = c("a", "b", "a", "c"))
+  expect_equal(
+    get_unique_rows(df, c("A", "B")),
+    data.frame(A = c(1, 2, 2), B = c("a", "b", "c"))
+  )
+  expect_equal(
+    get_unique_rows(df, "A"),
+    data.frame(A = 1:2)
+  )
+  expect_equal(
+    get_unique_rows(data.frame(A = 1:2), "A"),
+    data.frame(A = 1:2)
+  )
 })
