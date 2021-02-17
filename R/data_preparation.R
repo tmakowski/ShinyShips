a0f55fdbf175fc1a0ffcb5675042cbdc9f6690ef
tmakowski <tymek.makowski@gmail.com>
2021-02-17 20:20:03 +0100

    Add get_ships_distances()

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -12,3 +12,57 @@ get_unique_type_name_pairs <- function(ships_data_path) {
   ships <- data.table::fread(ships_data_path)
   get_unique_rows(ships, c("ship_type", "SHIPNAME"))
 }
+
+#' Extract max distance per ship between consecutive observations
+#'
+#' @param ships_data_path A path to ships.csv input data.
+#'
+#' @return A `data.table` with SHIP_ID, travel start point, end point, and
+#'   travelled distance for the maximum travelled distance.
+#'
+#' @import data.table
+get_ships_distances <- function(ships_data_path) {
+  assertthat::assert_that(
+    is.character(ships_data_path), length(ships_data_path) == 1,
+    file.exists(ships_data_path)
+  )
+
+  ships <- data.table::fread(ships_data_path)
+  ships_coords <- ships[order(DATETIME), .(SHIP_ID, LAT, LON)]
+
+  # Removing rows where the ship has not moved and omitting stationary ships
+  unique_coords <- unique(ships_coords)
+  moving_ships <- unique_coords[, .(LAT, LON, n = .N), by = .(SHIP_ID)][n > 1]
+
+  sailed_dists <- moving_ships[,
+    .(
+      dist      = calc_lag_distances(LAT, LON),
+      lat_start = data.table::shift(LAT),
+      lon_start = data.table::shift(LON),
+      lat_end   = LAT,
+      lon_end   = LON
+    ),
+    by = .(SHIP_ID)
+  ]
+
+  max_dists <- sailed_dists[,
+    .(
+      max_dist = max(dist, na.rm = TRUE)
+    ),
+    by = .(SHIP_ID)
+  ]
+
+  max_dists_coords <- max_dists[sailed_dists, on = .(SHIP_ID)][max_dist == dist]
+
+  # Rows are sorted by DATETIME therefore, tail(..., 1) is latest observation
+  max_dists_coords[,
+    .(
+      dist      = tail(dist, 1),
+      lat_start = tail(lat_start, 1),
+      lon_start = tail(lon_start, 1),
+      lat_end   = tail(lat_end, 1),
+      lon_end   = tail(lon_end, 1)
+    ),
+    by = SHIP_ID
+  ]
+}
