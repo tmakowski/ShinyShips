5a30e236f877e9898dbf8373fda483a8e8a1b5e5
tmakowski <tymek.makowski@gmail.com>
2021-02-17 22:25:46 +0100

    Add ship selection UI

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,5 +1,15 @@
 app_ui <- function() {
   semanticPage(
-    h1("Hello world!")
+    h1("Hello world!"),
+    ui_ship_selection()
+  )
+}
+
+ui_ship_selection <- function() {
+  ns <- NS(NS_SHIP_SELECTION)
+
+  split_layout(
+    dropdown_input(ns("type"), ships$types),
+    dropdown_input(ns("name"), NULL)
   )
 }
