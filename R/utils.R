dd221d5cf7795297966f9af748100c460f07c2fb
tmakowski <tymek.makowski@gmail.com>
2021-02-17 21:06:13 +0100

    Change to data.table

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,18 @@
 #' Get unique rows from a `data.frame`
 #'
-#' @param df An input `data.frame`.
+#' @param dt An input `data.frame`.
 #' @param col_names A vector of column names to subset from `df`.
 #'
 #' @return A `data.frame` with unique rows.
-get_unique_rows <- function(df, col_names) {
+get_unique_rows <- function(dt, col_names) {
   assertthat::assert_that(
-    is.data.frame(df),
+    is.data.table(dt),
     is.vector(col_names),
     length(col_names) > 0,
-    all(col_names %in% colnames(df))
+    all(col_names %in% colnames(dt))
   )
 
-  unique_rows <- unique(df[, col_names, drop = FALSE])
+  unique_rows <- unique(dt[, ..col_names, drop = FALSE])
   rownames(unique_rows) <- NULL
   unique_rows
 }
