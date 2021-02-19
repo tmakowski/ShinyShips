ec332b12e9685d342a7ea2114c40397a7da4518e
tmakowski <tymek.makowski@gmail.com>
2021-02-19 18:37:25 +0100

    Add server_ship_selection() server module

--- R/app_server.R
+++ R/app_server.R
@@ -1,2 +1,10 @@
 app_server <- function(input, output, session) {
+  moduleServer(NS_SHIP_SELECTION, server_ship_selection, session)
+}
+
+server_ship_selection <- function(input, output, session) {
+  observe({
+    type <- input$type
+    update_dropdown_input(session, "name", ships$type_names[[type]])
+  })
 }
