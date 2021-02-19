a82a81aeb3ef70faeca19cef5ec86741f230915c
tmakowski <tymek.makowski@gmail.com>
2021-02-19 21:15:33 +0100

    Add get_ships_distances() tests

--- tests/testthat/test_get_type_names_mapping.R
+++ tests/testthat/test_get_type_names_mapping.R
@@ -26,3 +26,63 @@ test_that("test get_type_names_mapping", {
   res3 <- list("TypeA" = "Ship1", "TypeB" = c("Ship1", "Ship2"))
   expect_equal(get_type_names_mapping(ships_data3), res3)
 })
+
+test_that("test get_ships_distances", {
+  # Assert check
+  expect_error(get_ships_distances(data.frame()))
+  expect_error(get_ships_distances(data.table::data.table()))
+
+  # Calculation check
+  # Highest value selection
+  ships_data1 <- data.table::data.table(
+    SHIPNAME = "Ship1",
+    DATETIME = 1:3,
+    LAT = c(0, 1, 10),
+    LON = c(0, 1, 10)
+  )
+  res1 <- data.table::data.table(
+    SHIPNAME = "Ship1",
+    dist = geosphere::distGeo(c(1, 1), c(10, 10)),
+    lat_start = 1,
+    lon_start = 1,
+    lat_end = 10,
+    lon_end = 10
+  )
+  expect_equal(get_ships_distances(ships_data1), res1)
+
+  # Ommiting ships with too few observations
+  ships_data2 <- data.table::rbindlist(list(
+    ships_data1,
+    data.table::data.table(SHIPNAME = "Ship2", DATETIME = 1, LAT = 0, LON = 0)
+  ))
+  expect_equal(get_ships_distances(ships_data2), res1)
+
+  # Ommiting consecutive observations without moving
+  ships_data3 <- data.table::rbindlist(list(
+    ships_data1,
+    data.table::data.table(
+      SHIPNAME = "Ship2",
+      DATETIME = 1:3,
+      LAT = c(0, 0, 0),
+      LON = c(0, 0, 0)
+    )
+  ))
+  expect_equal(get_ships_distances(ships_data3), res1)
+
+  # Selecting latest observation
+  ships_data4 <- data.table::data.table(
+    SHIPNAME = "Ship1",
+    DATETIME = 1:3,
+    LAT = c(0, 0, 0),
+    LON = c(0, 1, 0)
+  )
+  res4 <- data.table::data.table(
+    SHIPNAME = "Ship1",
+    dist = geosphere::distGeo(c(0, 1), c(0, 0)),
+    lat_start = 0,
+    lon_start = 1,
+    lat_end = 0,
+    lon_end = 0
+  )
+  expect_equal(get_ships_distances(ships_data4), res4)
+})
