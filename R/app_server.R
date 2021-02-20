e586b18404354bbaf1c3bd0de375ca630364efd0
tmakowski <tymek.makowski@gmail.com>
2021-02-20 22:15:14 +0100

    Disable selecting first ship name automatically

--- R/app_server.R
+++ R/app_server.R
@@ -62,7 +62,7 @@ app_server <- function(input, output, session) {
 server_ship_selection <- function(input, output, session) {
   observe({
     type <- input$type
-    update_dropdown_input(session, "name", ships$type_names[[type]])
+    update_dropdown_input(session, "name", ships$type_names[[type]], value = NA)
   })
 
   reactive(input$name)
